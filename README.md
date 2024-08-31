dcfa169fecc1fba49a4569a43bfe3b62bd3d3356
jossef <jossef12@gmail.com>
2024-08-31 13:57:57 +0300

    feat: simplifying project to gh-pages

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-# stonks
-cron based crawler for selected stock values
+# Stonks
 
-I'm not responsible for any financial loss you may incur by using this tool. Use at your own risk.
+This project is a cron based crawler for selected stock values
+
+As some symbols/tickers require פליקפלקים באוויר (a phrase in Hebrew means a lot of effort) in order to be fetched, I created this as a one-stop-shop project as a source of information for my GoogleSheets portfolio.
+
+Created for personal use. I'm not responsible for any financial loss you may incur by using this project. Use at your own risk.
+
+As I always like to say, if it looks stupid but it works, it ain't stupid.
--- main.py
+++ main.py
@@ -11,14 +11,26 @@ from seleniumwire import webdriver
 
 chromedriver_autoinstaller.install()
 
-CHROME_USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'
-
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 SYMBOLS_DIR = os.path.join(SCRIPT_DIR, 'symbols')
+DIST_DIR = os.path.join(SCRIPT_DIR, 'dist')
+
+
+def get_latest_user_agent(operating_system='windows', browser='chrome'):
+    url = f'https://jnrbsn.github.io/user-agents/user-agents.json'
+    r = requests.get(url)
+    r.raise_for_status()
+    user_agents = r.json()
+
+    for user_agent in user_agents:
+        if operating_system.lower() in user_agent.lower() and browser.lower() in user_agent.lower():
+            return user_agent
+
+    return None
 
 
 def main():
-    for symbol_track_file_path in glob.glob(os.path.join(SYMBOLS_DIR, '**', 'track.json'), recursive=True):
+    for symbol_track_file_path in glob.glob(os.path.join(SYMBOLS_DIR, '*.json'), recursive=True):
 
         with open(symbol_track_file_path) as f:
             symbol_track_info = json.load(f)
@@ -29,10 +41,11 @@ def main():
         symbol_id = symbol_track_info['id']
         symbol = symbol_track_info['symbol']
         currency = symbol_track_info['currency']
+        user_agent_header = get_latest_user_agent(operating_system='windows', browser='chrome')
 
         if symbol_track_info['source'] == 'justetf':
             url = f'https://www.justetf.com/api/etfs/{symbol}/quote?locale=en&currency={currency}&isin={symbol}'
-            r = requests.get(url, headers={'User-Agent': CHROME_USER_AGENT, 'Accept': 'application/json'})
+            r = requests.get(url, headers={'User-Agent': user_agent_header, 'Accept': 'application/json'})
             r.raise_for_status()
             symbol_info = r.json()
             symbol_price = symbol_info['latestQuote']['raw']
@@ -40,9 +53,9 @@ def main():
 
         elif symbol_track_info['source'] == 'yahoo_finance':
             ticker_yahoo = yf.Ticker(symbol)
-            symbol_track_info = ticker_yahoo.history()
-            symbol_price = symbol_track_info['Close'].iloc[-1]
-            symbol_price_date = symbol_track_info['Close'].index[-1]
+            symbol_info = ticker_yahoo.history()
+            symbol_price = symbol_info['Close'].iloc[-1]
+            symbol_price_date = symbol_info['Close'].index[-1]
             symbol_price_date = datetime.datetime.strftime(symbol_price_date, '%Y-%m-%d')
 
         elif symbol_track_info['source'] == 'issa':
@@ -67,7 +80,7 @@ def main():
                             response = brotli.decompress(request.response.body)
                             response = response.decode('utf-8')
                             response = json.loads(response)
-                            symbol_price = response['SellPrice'] / 100 # ILA -> ILS
+                            symbol_price = response['SellPrice'] / 100  # ILA -> ILS
                             symbol_price_date = response['RelevantDate']
                             symbol_price_date = datetime.datetime.fromisoformat(symbol_price_date).strftime('%Y-%m-%d')
 
@@ -75,17 +88,29 @@ def main():
                             response = brotli.decompress(request.response.body)
                             response = response.decode('utf-8')
                             response = json.loads(response)
-                            symbol_price = response['LastRate'] / 100 # ILA -> ILS
+                            symbol_price = response['LastRate'] / 100  # ILA -> ILS
                             symbol_price_date = response['TradeDate']
                             symbol_price_date = datetime.datetime.strptime(symbol_price_date, "%d/%m/%Y").strftime('%Y-%m-%d')
- 
-
 
         if not symbol_price:
             raise Exception(f'Failed to get price for {symbol}')
 
-        with open(os.path.join(os.path.dirname(symbol_track_file_path), 'price'), 'w+') as f:
-            f.write(f'{symbol_price}')
+        symbol_dist_dir = os.path.join(DIST_DIR, symbol_id)
+        os.makedirs(symbol_dist_dir, exist_ok=True)
+        symbol_track_info['price'] = symbol_price
+        symbol_track_info['price_date'] = symbol_price_date
+
+        with open(os.path.join(symbol_dist_dir, 'price'), 'w+') as f:
+            f.write(str(symbol_price))
+
+        with open(os.path.join(symbol_dist_dir, 'currency'), 'w+') as f:
+            f.write(currency)
+
+        with open(os.path.join(symbol_dist_dir, 'date'), 'w+') as f:
+            f.write(symbol_price_date)
+
+        with open(os.path.join(symbol_dist_dir, 'info.json'), 'w+') as f:
+            json.dump(symbol_track_info, f)
 
         print(symbol_id, symbol_price_date, symbol_price, currency)
 
